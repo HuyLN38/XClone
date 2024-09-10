386b0466da105ef43705b4355f9b79fe88ec6221
Le Tuan Dat <147417920+Vuighe22@users.noreply.github.com>
2024-09-10 11:03:26 +0700

    pre communities feature

---
+++ app/src/main/java/vn/edu/usth/x/community/CommunityFragment.java
@@ -0,0 +1,43 @@
+package vn.edu.usth.x.community;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.Arrays;
+import vn.edu.usth.x.R;
+
+public class CommunityFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_community, container, false);
+
+        // Set up the RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.Community_recyclerView);
+
+        // Set a layout manager for the RecyclerView
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Initialize the adapter and set it to the RecyclerView
+        CommunityTweetAdapter adapter = new CommunityTweetAdapter(getTweetData());
+        recyclerView.setAdapter(adapter);
+
+        return view; // Return the inflated view
+    }
+
+    private ArrayList<CommunityTweet> getTweetData() {
+        // Return an ArrayList of tweets
+        return new ArrayList<>(
+                Arrays.asList(
+                        new CommunityTweet("Football", R.drawable.avatar, "Elon Musk", "elonmusk", "Doge", "2h", R.drawable.avatar),
+                        new CommunityTweet("Book Club", R.drawable.johnnysins, "Johnny Sins", "johnnysins", "I'm a plumber", "3h", R.drawable.johnnysins)
+                )
+        );
+    }
+}
---
+++ app/src/main/java/vn/edu/usth/x/community/CommunityTweetAdapter.java
@@ -0,0 +1,70 @@
+package vn.edu.usth.x.community;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import vn.edu.usth.x.R;
+
+public class CommunityTweetAdapter extends RecyclerView.Adapter<CommunityTweetAdapter.MyCommunityHolder> {
+    private ArrayList<CommunityTweet> communityList;
+
+    public CommunityTweetAdapter(ArrayList<CommunityTweet> communityList){
+        this.communityList = communityList;
+    }
+
+    @NonNull
+    @Override
+    public CommunityTweetAdapter.MyCommunityHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.item_community_tweet,parent,false);
+        return new CommunityTweetAdapter.MyCommunityHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CommunityTweetAdapter.MyCommunityHolder holder, int position) {
+        CommunityTweet cmTweet = communityList.get(position);
+        holder.community_name.setText(cmTweet.getCommunity());
+        holder.community_username.setText(cmTweet.getUsername());
+        holder.community_tweetlink.setText(cmTweet.getTweetlink());
+        holder.community_tweetText.setText(cmTweet.getTweetText());
+        holder.community_time.setText(cmTweet.getTime());
+        holder.community_image.setImageResource(cmTweet.getImage());
+        holder.community_avatar.setImageResource(cmTweet.getAvatar());
+    }
+
+    @Override
+    public int getItemCount() {
+        return communityList.size();
+    }
+
+    public static class MyCommunityHolder extends RecyclerView.ViewHolder{
+        public TextView community_name;
+        public CircleImageView community_avatar;
+        public TextView community_username;
+        public TextView community_tweetText;
+        public TextView community_tweetlink;
+        public TextView community_time;
+        public ImageView community_image;
+
+        public MyCommunityHolder(@NonNull View itemView) {
+            super(itemView);
+            community_name = itemView.findViewById(R.id.community_name);
+            community_avatar = itemView.findViewById(R.id.community_avatar);
+            community_username = itemView.findViewById(R.id.community_username);
+            community_tweetText = itemView.findViewById(R.id.community_tweet_text);
+            community_tweetlink = itemView.findViewById(R.id.community_tweetlink);
+            community_time = itemView.findViewById(R.id.community_time);
+            community_image = itemView.findViewById(R.id.community_image);
+        }
+    }
+}
