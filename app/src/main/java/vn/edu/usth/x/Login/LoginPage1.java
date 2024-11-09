46a91bbc2f0070abf41f7a158ef4b602232d5085
HuyLN <lynhathuy38@gmail.com>
2024-11-09 15:21:41 +0700

    Failed login anouce

--- app/src/main/java/vn/edu/usth/x/Login/LoginPage1.java
+++ app/src/main/java/vn/edu/usth/x/Login/LoginPage1.java
@@ -2,13 +2,13 @@ package vn.edu.usth.x.Login;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -35,6 +35,7 @@ public class LoginPage1 extends AppCompatActivity {
     private EditText indentify;
     private EditText passwordEditText;
     private Button loginButton;
+    private TextView loginStatus;
 
 
     @Override
@@ -45,6 +46,8 @@ public class LoginPage1 extends AppCompatActivity {
         indentify = findViewById(R.id.name);
         passwordEditText = findViewById(R.id.password);
         loginButton = findViewById(R.id.Login);
+        loginStatus = findViewById(R.id.loginStatus);
+
 
         loginButton.setOnClickListener(v -> {
             try {
@@ -99,6 +102,7 @@ public class LoginPage1 extends AppCompatActivity {
 
                     return true;
                 } else {
+                    loginStatus.setVisibility(View.VISIBLE);
                     Log.d("LoginTask", "Response message: " + conn.getResponseMessage());
                     return false;
                 }
